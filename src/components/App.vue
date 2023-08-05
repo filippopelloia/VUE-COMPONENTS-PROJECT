4cd700937c47af6a1ba44570832d7067f0d4aa74
Dev1 - filip <pelloiafilippo@live.com>
2023-08-05 23:02:39 +0200

    first commit august 5th

---
+++ src/components/App.vue
@@ -0,0 +1,14 @@
+<template>
+    <p>Simple text</p>
+    <h1>{{ marando }}</h1>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                marando: 'Io me ciamo Luigi Marando!!'
+            }
+        }
+    }
+</script>
